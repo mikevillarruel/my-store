c37b486ec10c7e15d7162f5b211c1fbf46779b16
Michael Villarruel <alejovillarruel@gmail.com>
2023-05-18 19:01:34 -0500

    feat: Create product form
    - Create base template
    - Create create_product template
    - Add Bootstrap v5.2.3 as our frontend toolkit
    - Change the respective view and form according to previous changes made to models

--- products/forms.py
+++ products/forms.py
@@ -4,11 +4,16 @@ from .models import Product
 
 
 class ProductForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs['class'] = "form-control"
+
     class Meta:
         model = Product
         fields = '__all__'
         exclude = ['created_at', 'modified_at', 'deleted_at', 'images']
 
-    path = forms.ImageField(widget=forms.ClearableFileInput(attrs={
+    images = forms.ImageField(widget=forms.ClearableFileInput(attrs={
         "multiple": True
     }))
--- products/views.py
+++ products/views.py
@@ -9,17 +9,17 @@ def create_product(request):
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
             product = form.save()
-            images = request.FILES.getlist('path')
+            images = request.FILES.getlist('images')
 
             for image in images:
                 product.images.create(path=image)
 
             return redirect('create_product')
         else:
-            return render(request, 'create_product.html', {
+            return render(request, 'products/create_product.html', {
                 'form': form,
             })
     else:
-        return render(request, 'create_product.html', {
+        return render(request, 'products/create_product.html', {
             'form': ProductForm()
         })
