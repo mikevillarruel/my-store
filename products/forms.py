bc27aff509681f5195dbb0a8a6ede31058f8e6ff
Michael Villarruel <alejovillarruel@gmail.com>
2023-05-24 18:02:57 -0500

    feat: Improve form and template to create product
    - Show which fields are required
    - Set range to discount and stock fields
    - Remove color field in Product model
    - Add media folder to .gitignore

--- products/forms.py
+++ products/forms.py
@@ -6,14 +6,20 @@ from .models import Product
 class ProductForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
         for field in self.fields.values():
             field.widget.attrs['class'] = "form-control"
+
+        self.fields['discount'].widget.attrs["max"] = 100
+        self.fields['discount'].widget.attrs["min"] = 0
+
+        self.fields['stock'].widget.attrs["min"] = 1
 
     class Meta:
         model = Product
         fields = '__all__'
         exclude = ['created_at', 'modified_at', 'deleted_at', 'images']
 
-    images = forms.ImageField(widget=forms.ClearableFileInput(attrs={
+    images = forms.ImageField(label='Images', widget=forms.ClearableFileInput(attrs={
         "multiple": True
     }))
--- products/models.py
+++ products/models.py
@@ -10,15 +10,14 @@ class Product(models.Model):
     name = models.CharField(max_length=64)
     description = models.TextField()
     size = models.CharField(max_length=16)
-    color = models.CharField(max_length=16)
-    brand = models.CharField(max_length=64)
-    material = models.CharField(max_length=64, null=True)
+    brand = models.CharField(max_length=64, null=True, blank=True)
+    material = models.CharField(max_length=64, null=True, blank=True)
     stock = models.IntegerField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    discount = models.IntegerField(null=True)
+    discount = models.IntegerField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
-    deleted_at = models.DateTimeField(null=True)
+    deleted_at = models.DateTimeField(null=True, blank=True)
     images = models.ManyToManyField(Image)
 
 
@@ -26,11 +25,11 @@ class Order(models.Model):
     total = models.DecimalField(max_digits=10, decimal_places=2)
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
-    deleted_at = models.DateTimeField(null=True)
+    deleted_at = models.DateTimeField(null=True, blank=True)
 
 
 class OrderItem(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
     quantity = models.IntegerField()
-    discount = models.IntegerField(null=True)
+    discount = models.IntegerField(null=True, blank=True)
