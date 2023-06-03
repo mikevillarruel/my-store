e4595bd92992c722fb7724b1c7a803fc1ef3fa5a
Michael Villarruel <alejovillarruel@gmail.com>
2023-06-02 23:06:32 -0500

    refactor: Move Order and OrderItem models to sales app

--- products/models.py
+++ products/models.py
@@ -22,17 +22,3 @@ class Product(models.Model):
     modified_at = models.DateTimeField(auto_now=True)
     deleted_at = models.DateTimeField(null=True, blank=True)
     images = models.ManyToManyField(Image)
-
-
-class Order(models.Model):
-    total = models.DecimalField(max_digits=10, decimal_places=2)
-    created_at = models.DateTimeField(auto_now_add=True)
-    modified_at = models.DateTimeField(auto_now=True)
-    deleted_at = models.DateTimeField(null=True, blank=True)
-
-
-class OrderItem(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    order = models.ForeignKey(Order, on_delete=models.CASCADE)
-    quantity = models.IntegerField()
-    discount = models.IntegerField(null=True, blank=True)
--- sales/models.py
+++ sales/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+from products.models import Product
+
+
 # Create your models here.
+class Order(models.Model):
+    total = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(null=True, blank=True)
+
+
+class OrderItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    discount = models.IntegerField(null=True, blank=True)
