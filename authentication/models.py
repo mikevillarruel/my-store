61c58a4e37057f2f7664a0a006596822b1d6aac4
Michael Villarruel <alejovillarruel@gmail.com>
2023-06-11 21:41:58 -0500

    feat: Set the picture field of User model to non-mandatory and delete the file when it changes
    - Remove default value of picture field in User model
    - Set null and blank properties of picture field as True
    - Override save method of User model in order to delete the file associated with picture field when it changes
    - Add default_avatar image to static files folder
    - Change account template in order to show default_avatar image as profile picture if picture field is not set

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,6 @@
+import os
+
+from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
@@ -18,4 +21,14 @@ class User(AbstractUser):
         max_length=128,
         validators=[password_validator],
     )
-    picture = models.ImageField(upload_to='profile_images/', default='profile_images/default.png')
+    picture = models.ImageField(upload_to='profile_pictures/', null=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        if self.pk:
+            old_picture = User.objects.get(pk=self.pk).picture
+            if old_picture and old_picture != self.picture:
+                try:
+                    os.remove(os.path.join(settings.MEDIA_ROOT, old_picture.path))
+                except FileNotFoundError:
+                    pass
+        super().save(*args, **kwargs)
